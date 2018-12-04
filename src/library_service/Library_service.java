c23ae8dc2a2ccd5d98a94031f7af848e21987a44
Govardhan Padmanabhan <39567171+GovardhanPadmanabhan@users.noreply.github.com>
2018-12-04 23:17:47 +0800

    Add files via upload

---
+++ src/library_service/Library_service.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library_service;
+
+/**
+ *
+ * @author Govardhan
+ */
+
+import java.io.FileWriter;
+import java.io.FileReader;
+
+public class Library_service {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    
+    public static void main(String[] args) {
+        new main_page().setVisible(true);
+        //this.setVisible(false);
+    }
+    
+}
